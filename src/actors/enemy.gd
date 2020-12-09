12905258bb41d0f4795d69a45a60211417e3b77e
Thiago de Jesus <99thiagodejesus@gmail.com>
2020-12-09 10:59:45 -0300

    Added Enemy and Camera

---
+++ src/actors/enemy.gd
@@ -0,0 +1,13 @@
+extends "res://src/actors/actor.gd"
+
+func _ready() -> void:
+	set_physics_process(false)
+	velocity.x = -speed.x
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	if is_on_wall():
+		velocity.x *= -1.0
+	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
+	
+	
