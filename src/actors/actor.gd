0db769ae1c701e96253d14ec1ec35b2976758946
Thiago de Jesus <99thiagodejesus@gmail.com>
2020-12-09 09:24:57 -0300

    Added Jump

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 class_name actor
 
+const FLOOR_NORMAL: = Vector2.UP
+
 export var speed: = Vector2(800.0, 1000.0)
 export var gravity: = 3000.0
 var velocity: = Vector2.ZERO
 
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+
